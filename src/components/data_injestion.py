1eed187e4a51707e7346d4cdcec1f41339eaff4b
Sacag08 <sachitagarwal98@gmail.com>
2024-09-14 19:39:33 +0530

    Jupyter NoteBook Creation

---
+++ src/components/data_injestion.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+import numpy as np
+import pickle
+import sys
+from dataclasses import dataclass
+
+from src.logger import logging
+from src.exception import CustomException
+
+from sklearn.model_selection import train_test_split
+@dataclass
+class DataInjestionConfig:
+    train_data_path : str = os.path.join('artifacts','train.csv')
+    test_data_path : str = os.path.join('artifacts','test.csv')
+    val_data_path : str = os.path.join('artifacts','val.csv')
+
+
+class DataInjestion:
+    def __init__(self):
+        self.DataInjestionConfig = DataInjestionConfig()
+    
+    def initiate_data_injestion(self):
+        logging.info('Initiated Data Injestion')
+        try:
+            data = pd.read_csv('C:/Projects/Used Car Price prediction/src/Notebook/data/train.csv')
+            os.makedirs(os.path.dirname(self.DataInjestionConfig.train_data_path),exist_ok=True)
+            data.to_csv(DataInjestionConfig.train_data_path)
+            logging.info('Read and wrote the data to artifacts')
+            train_data,test_data = train_test_split(data,test_size=0.3,random_state=41)
+            
+            train_data.to_csv(DataInjestionConfig.train_data"C:\Projects\Used Car Price prediction\src\components\data_injestion.py"_path)
+            test_data.to_csv(DataInjestionConfig.test_data_path)
+            logging.info('Splitted the data in test and train datasets and pasted to artifacts')
+            return (
+                self.DataInjestionConfig.train_data_path,
+                self.DataInjestionConfig.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__ =="__main__":
+    obj = DataInjestion()
+    print(obj.initiate_data_injestion())
+            
